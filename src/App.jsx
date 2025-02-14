870f6f491f2011456c8e033b168078e40ee39264
Your Name <sachinbhavar@gmail.com>
2025-02-14 16:02:53 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import Autocomplete from "./components/Autocomplete";
 
 function App() {
     return (
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <h1>Autocomplete</h1>
+            <h1>Autocoplete</h1>
+            <Autocomplete />
         </div>
     );
 }
